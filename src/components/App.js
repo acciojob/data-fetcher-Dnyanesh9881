f2636e11ca47880d55a5fa1f941ff103092b580a
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-04-05 07:35:02 +0000

    asdsdas

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[data, setData]=useState({});
+
+  useEffect(()=>{
+    fetch("https://dummyjson.com/products")
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data);
+      setData({...data})})
+    .catch((error)=>{console.log(error)})
+  },[])
   return (
     <div>
         {/* Do not remove the main div */}
+        <pre>
+           <pre>{JSON.stringify(data, null, 2)}</pre>
+        </pre>
     </div>
   )
 }
