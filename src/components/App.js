004acbc697fdc275637c7e90e814277aa56a6ead
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-04-05 08:09:00 +0000

    asd

--- src/components/App.js
+++ src/components/App.js
@@ -6,22 +6,25 @@ const App = () => {
   const[data, setData]=useState({});
   const [error, setError]=useState(false);
 
-  useEffect(()=>{
-    fetch("https://dummyjson.com/products")
-    .then((res)=>res.json())
-    .then((data)=>{
-      console.log(data);
-      setData({...data})})
-    .catch((error)=>{
-      setError(true);
-            console.log(error)})
-  },[])
+  // useEffect(()=>{
+  //   fetch("https://dummyjson.com/products")
+  //   .then((res)=>res.json())
+  //   .then((data)=>{
+  //     console.log(data);
+  //     setError(false);
+  //     setData({...data})
+  //   })
+  //   .catch((error)=>{
+  //     setError(true);
+  //           // console.log(error)
+  //         })
+  // },[])
   return (
     <div>
         {/* Do not remove the main div */}
         <div>{
            
-           error ?<p>An error occurred: while fetching data</p> : Object.keys(data).length===0?<p>No data found</p>:<div>
+           error ?<p>An error occurred: while fetching data</p> : Object.keys(data).length===0?<pre>No data found</pre>:<div>
            <h1>Data Fetched from API</h1>
            <pre>{JSON.stringify(data, null, 2)}</pre></div> 
           }
