3111ca55aec7eb2f377c6fd2e9aa2b5031900589
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-04-05 08:46:24 +0000

    final

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [error, setError]=useState(false);
 
   useEffect(()=>{
-    fetch("https://dummyjson.cm/products")
+    fetch("https://dummyjson.com/products")
     .then((res)=>res.json())
     .then((data)=>{
       console.log(data);
